0b1500b43f86555390ec97b1f7ed0cebe653498d
Jorge <blesagr@technikum-wien.at>
2021-05-18 23:41:23 +0200

    main type corrected

--- main.cpp
+++ main.cpp
@@ -99,8 +99,6 @@ int main(int argc, char** argv){
                     finished
                 }
             }
-            if( unfinished == false )
-                break;
         }
     }    
 
