2b694dbf6666d1d71c5397d0b03e98bbf09918e8
Jorge <blesagr@technikum-wien.at>
2021-11-30 11:06:01 +0100

    CI corrected

--- Tests/CRCLnamespace_tests/testsServer.cpp
+++ Tests/CRCLnamespace_tests/testsServer.cpp
@@ -1,5 +1,7 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch_all.hpp>
+//#include <catch2/catch_test_macros.hpp>
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch2/catch.hpp>
+
 
 #include <open62541/plugin/log_stdout.h>
 #include <open62541/server.h>
--- tools/ci.sh
+++ tools/ci.sh
@@ -70,17 +70,49 @@ function build_tpm_tool {
 # Build Release Version #
 #########################
 
+function build_open62541 {
+    mkdir SamyPlugins_Template_Cpp/open62541_v1.2_unions_fixed/build;
+    cd SamyPlugins_Template_Cpp/open62541_v1.2_unions_fixed/build
+    rm -rf *
+    cmake -DUA_NAMESPACE_ZERO=FULL \
+      -DUA_ENABLE_SUBSCRIPTIONS=ON \
+      -DUA_ENABLE_SUBSCRIPTIONS_EVENTS=ON \
+      -DUA_ENABLE_DISCOVERY=ON \
+      -DUA_ENABLE_DISCOVERY_MULTICAST=ON \
+      -DUA_ENABLE_DA=ON \
+      -DUA_ENABLE_METHODCALLS=ON \
+      -DUA_ENABLE_NODEMANAGEMENT=ON \
+      -DUA_ENABLE_MICRO_EMB_DEV_PROFILE=OFF \
+      -DUA_ENABLE_PUBSUB=ON \
+      -DUA_ENABLE_PUBSUB_ETH_UADP=ON \
+      -DUA_ENABLE_PUBSUB_ETH_UADP_ETF=ON \
+      -DUA_ENABLE_PUBSUB_FILE_CONFIG=ON \
+      -DUA_ENABLE_PUBSUB_INFORMATIONMODEL=ON \
+      -DUA_ENABLE_PUBSUB_INFORMATIONMODEL_METHODS=ON \
+      -DUA_ENABLE_PUBSUB_MONITORING=ON \
+      -DUA_ENABLE_PUBSUB_MQTT=ON \
+      -DUA_ENABLE_JSON_ENCODING=ON \
+      ..
+    make ${MAKEOPTS}
+    sudo make install
+}
+
+function build_catch2 {
+    cd Catch2
+    mkdir -p build && cd build
+    rm -rf *
+    cmake ..
+    make ${MAKEOPTS}
+    sudo make install
+}
+
 function build_release {
-    mkdir -p build; cd build; rm -rf *
-    cmake -DBUILD_SHARED_LIBS=ON \
-          -DUA_ENABLE_ENCRYPTION=MBEDTLS \
-          -DUA_ENABLE_SUBSCRIPTIONS_EVENTS=ON \
-          -DUA_ENABLE_HISTORIZING=ON \
-          -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-          -DUA_BUILD_EXAMPLES=ON \
-          ..
+    mkdir -p build && cd build
+    rm -rf *
+    cmake ..
     make ${MAKEOPTS}
 }
+
 
 ######################
 # Build Amalgamation #
@@ -118,6 +150,7 @@ function build_amalgamation_mt {
           ..
     make ${MAKEOPTS}
 }
+
 
 ############################
 # Build and Run Unit Tests #
