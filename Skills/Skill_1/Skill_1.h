90539d7d1d8bb425790e7e32fb284dae13295846
Jorge <blesagr@technikum-wien.at>
2022-10-12 12:54:05 +0200

    Add comments to skill1 code

--- Skills/Skill_1/Skill_1.h
+++ Skills/Skill_1/Skill_1.h
@@ -7,14 +7,6 @@
 #include <skillsScriptingAPI.h>
 
 extern "C" {
-/*
-        void SAMYCoreAPI_dummy();
-        void SAMYCoreAPI_dummyCommandState_Running();
-        void SAMYCoreAPI_dummyCommandState_Pending();
-        void SAMYCoreAPI_dummyCommandState_Awaiting();
-        void SAMYCoreAPI_dummyWait();
-*/
-
         void testFunction2( UA_UInt32 const * const param1, UA_UInt32 const * const param2 );
         void testFunction( UA_UInt32 param1, UA_UInt32 param2 );
 } 
